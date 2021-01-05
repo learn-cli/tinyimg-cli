bf82b08eac72bf58f4f9f416e23a829f7fdbace8
liangchaofei <chaofei_liang@126.com>
2021-01-05 18:21:52 +0800

    feat: init

---
+++ a.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-05 18:21:27
+ * @LastEditTime: 2021-01-05 18:21:31
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /tinyimg-cli/a.js
+ */
+console.log('a')
